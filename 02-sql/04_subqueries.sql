e0daaaebf44ce7d1d4c50df7590004f15c3d886e
O'Higgins Rodriguez Hernandez <ohigginsrodriguez@gmail.com>
2026-05-30 19:27:51 -0600

    feat: Add SQL subqueries exercises

--- 02-sql/04_subqueries.sql
+++ 02-sql/04_subqueries.sql
@@ -21,19 +21,37 @@ where total > (
 	) promedios
 );
 
+
 -- 1. Productos cuyo monto promedio supera 
 --    el promedio general de todos los montos
 
-select producto, monto, promedio, promedio_general
-from (
-	select 
-		producto,
-		monto,
-		
-)
+select producto, avg(monto) as promedio_producto
+from ventas
+group by producto
+having avg(monto) > (select avg(monto) from ventas)
+
 
 -- 2. El vendedor con el monto más alto en cada mes
 --    (usa subquery con ROW_NUMBER)
 
+select vendedor, monto, mes, monto_alto_mes
+from (
+select
+	vendedor,
+	monto,
+	mes,
+	row_number() over(partition by mes order by monto desc) as monto_alto_mes
+from ventas
+) subquery
+where subquery.monto_alto_mes = 1
+
 -- 3. Ventas donde el monto supera el total 
 --    de ventas de Luis
+
+select 
+	vendedor,
+	monto
+from ventas
+where monto > (select sum(monto) from ventas where vendedor = 'Luis')
+
+
